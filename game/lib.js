a0105aa00402ef7eafbcbcaceeb06c6e38559c58
Zafuzi <zacharyfoutz@gmail.com>
2024-02-03 11:51:11 -0800

    added a basic ecs and point hit for stations

---
+++ game/lib.js
@@ -0,0 +1,120 @@
+class Thing {
+	position = createVector(0, 0);
+	alive = true;
+	components = [];
+
+	constructor(_position, _alive) {
+		this.position = _position || this.position;
+		this.alive = _alive || this.alive;
+	}
+
+	update() {
+		if (!this.alive) {
+			return;
+		}
+
+		for (let i = 0; i < this.components.length; i++) {
+			this.components[i].update();
+		}
+	}
+
+	draw() {
+		if (!this.alive) {
+			return;
+		}
+
+		for (let i = 0; i < this.components.length; i++) {
+			this.components[i].draw();
+		}
+	}
+
+	kill() {
+		this.alive = false;
+	}
+}
+
+class Component {
+	thing = null;
+
+	constructor(_thing, _update, _draw) {
+		this.thing = _thing;
+		if (_update) {
+			this.update = _update;
+		}
+
+		if (_draw) {
+			this.draw = _draw;
+		}
+	}
+
+	update() {
+		if(!this.thing?.alive) {
+			return false;
+		}
+	}
+
+	draw() {
+		if(!this.thing?.alive) {
+			return false;
+		}
+	}
+}
+
+class cSimpleDraw extends Component {
+	draw() {
+		noStroke();
+		fill(this.thing.color || 255);
+		ellipse(this.thing.position.x, this.thing.position.y, this.thing.radius || 25, this.thing.radius || 25);
+	}
+}
+
+let mouseClickPosition = null;
+class cPointHit extends Component {
+	constructor(_thing) {
+		super(_thing);
+	}
+	update() {
+		super.update();
+
+		if(!mouseClickPosition) {
+			return false;
+		}
+
+		this.thing.selected = false;
+
+		if(isPointInCircle(mouseClickPosition, this.thing))
+		{
+			this.thing.selected = true;
+		}
+	}
+}
+
+class System {
+	things = []
+
+	update() {
+		for (let i = 0; i < this.things.length; i++) {
+			this.things[i].update();
+		}
+	}
+
+	draw() {
+		for (let i = 0; i < this.things.length; i++) {
+			this.things[i].draw();
+		}
+	}
+
+	add(thing) {
+		this.things.push(thing);
+	}
+}
+
+function isPointInCircle(point, circle) {
+	const distance = Math.floor(point.dist(circle.position));
+	return distance < circle.radius;
+}
+
+function optimizedHitTest(position, radius, otherPosition, otherRadius) {
+	const distance = position.dist(otherPosition);
+	return distance < radius + otherRadius;
+}
--- game/sketch.js
+++ game/sketch.js
@@ -13,17 +13,103 @@ function preload() {
 	music = loadSound("assets/space.mp3");
 }
 
+class Sun extends Thing {
+	constructor(_position) {
+		super(_position);
+		this.radius = 300;
+		this.color = color(255, 200, 40);
+		this.components.push(new cSimpleDraw(this));
+	}
+}
+
+class Planet extends Thing {
+	stations = new System();
+	constructor(_position, _radius, _color) {
+		super(_position);
+		this.radius = _radius || 40;
+		this.color = _color || color(random(255), random(255), random(255), 255);
+		this.components.push(new cSimpleDraw(this));
+	}
+
+	update() {
+		super.update();
+		this.stations.update();
+	}
+
+	draw() {
+		super.draw();
+		this.stations.draw();
+	}
+
+	addStation(_stationName) {
+		// _position is relative to the planet
+		const newPos = this.position.copy();
+		newPos.y += 100;
+
+		// add to y the number of stations * station radius
+		newPos.y += (this.stations.things.length * 40) * 2;
+
+		const newStation = new Station(newPos, _stationName);
+		this.stations.add(newStation);
+	}
+}
+
+class Station extends Thing {
+	selected = false;
+	radius = 20;
+	color = color(255, 255, 255);
+	components = [];
+	name = "Station";
+
+	constructor(_position, _name) {
+		super(_position);
+		this.name = _name || this.name;
+		this.components.push(new cSimpleDraw(this));
+		this.components.push(new cPointHit(this));
+	}
+
+	draw() {
+		super.draw();
+		textAlign(CENTER, CENTER);
+		fill(255);
+		text(this.name, this.position.x, this.position.y + this.radius*2);
+
+		if(this.selected) {
+			// 0074c7
+			fill(0, 74, 199, 100);
+			ellipse(this.position.x, this.position.y, this.radius*3, this.radius*3);
+		}
+	}
+}
+
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	ctx = canvas.elt.getContext("2d");
 
 	camera = createVector(0, 0);
 
-	// realistic mass of the sun scaled down
-	sun = new Body(400, createVector(-600, 0), color(255, 200, 40));
-	Planet(20, createVector(-150, 0), color(180, 180, 0, 255));
-	Planet(40, createVector(150, 0), color(0, 0, 180, 255));
-	Planet(100, createVector(600, 0), color(120, 40, 60, 255));
+	planets = new System();
+
+	sun = new Sun(createVector(-600, -200));
+
+	const kargnix = new Planet(createVector(-300, -200), 42, color(255, 80, 40));
+	kargnix.addStation("Kargnix Station 1");
+	planets.add(kargnix);
+
+	const bunglax = new Planet(createVector(-150, -200), 69, color(40, 80, 255));
+	bunglax.addStation("Bunglax Station 1");
+	bunglax.addStation("Bunglax Station 2");
+	bunglax.addStation("Bunglax Station 3");
+	planets.add(bunglax);
+
+	const evertrench = new Planet(createVector(50, -200), 101, color(40, 255, 80));
+	evertrench.addStation("Evertrench Station 1");
+	evertrench.addStation("Evertrench Station 2");
+	planets.add(evertrench);
+
+	const zorblax = new Planet(createVector(200, -200), 50, color(255, 255, 40));
+	zorblax.addStation("Zorblax Station 1");
+	planets.add(zorblax);
 
 	generateStars();
 }
@@ -33,41 +119,50 @@ function mousePressed() {
 	mouse.div(zoom);
 	mouse.sub(camera.copy());
 
-	if(!music.isPlaying()) {
+	if (!music.isPlaying()) {
 		music.setVolume(0.2);
 		music.loop();
 	}
 
-	if(mouseButton === LEFT) {
+	if (mouseButton === LEFT) {
+		mouseClickPosition = createVector(mouseX, mouseY);
+		mouseClickPosition.sub(width / 2, height / 2);
+		mouseClickPosition.div(zoom);
+		mouseClickPosition.sub(camera.copy());
 		// console.log(`mouseX: ${mouse.x}, mouseY: ${mouse.y}`);
 		// console.log(`cameraX: ${camera.x}, cameraY: ${camera.y}`);
 	}
 }
 
+function mouseReleased() {
+	mouseClickPosition = null;
+}
+
 function mouseDragged(dragEvent) {
-	camera.x += dragEvent.movementX/zoom;
-	camera.y += dragEvent.movementY/zoom;
+	camera.x += dragEvent.movementX / zoom;
+	camera.y += dragEvent.movementY / zoom;
 }
 
 function mouseWheel(event) {
 	zoom += (-event.deltaY * 0.001);
-	if(zoom < 0.1) {
+	if (zoom < 0.1) {
 		zoom = 0.1;
 	}
 
-	if(zoom > 2) {
+	if (zoom > 2) {
 		zoom = 2;
 	}
 }
 
 function keyPressed() {
 	console.log(key);
-	if(key === "`") {
+	if (key === "`") {
 		showOrbits = !showOrbits;
 	}
 }
 
 let tick = 0;
+
 function draw() {
 	tick++;
 
@@ -76,40 +171,15 @@ function draw() {
 
 	image(stars, 0, 0, width, height);
 
-	translate(width/2, height/2);
+	translate(width / 2, height / 2);
 	scale(zoom);
 	translate(camera.x, camera.y);
-
 
 	sun.update();
 	sun.draw();
 
-	for(let i = 0; i < planets.length; i++) {
-		planets[i].update();
-		planets[i].draw();
-	}
-
-}
-
-function Body(_mass, _position, _color) {
-	this.mass = _mass || 1;
-	this.position = _position || createVector(0, 0);
-	this.color = _color || color(random(255), random(255), random(255));
-	this.radius = this.mass;
-
-	this.draw = function() {
-		noStroke();
-		fill(this.color);
-		ellipse(this.position.x, this.position.y, this.radius, this.radius);
-	}
-
-	this.update = function() {}
-}
-
-
-function Planet(_radius, _position, _color) {
-	const planet = new Body(_radius, _position, _color);
-	planets.push(planet);
+	planets.update();
+	planets.draw();
 }
 
 function generateStars() {
@@ -118,7 +188,7 @@ function generateStars() {
 
 	stars.background(0);
 	stars.noStroke();
-	for(let i = 0; i < 10000; i++) {
+	for (let i = 0; i < 10000; i++) {
 		let radius = random(0.1, 3);
 		stars.fill(random(200, 255), random(200, 255), random(200, 255), random(10, 255));
 		stars.ellipse(random(-width, width), random(-height, height), radius, radius);
@@ -129,9 +199,4 @@ function generateStars() {
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 	generateStars();
-}
-
-function optimizedHitTest(position, radius, otherPosition, otherRadius) {
-	const distance = position.dist(otherPosition);
-	return distance < radius + otherRadius;
 }
